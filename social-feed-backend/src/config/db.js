d23760542a2350c63b31b42d71d6206f11e35940
Abdullah Al Rafee <abdullahalrafee@Abdullahs-MacBook-Air.local>
2026-04-02 22:39:18 +0600

    add: config file added

---
+++ social-feed-backend/src/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const dbConnection = await mongoose.connect(process.env.MONGODB_URI, {
+      maxPoolSize: 10,
+    });
+    console.log(`MongoDB connected: ${dbConnection.connection.host}`);
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
